8f603e95721d2b5fe912e8906b750dc0755a4787
IgorBelskyAn <belskyigor29@gmail.com>
2018-03-30 19:28:34 +0300

    add iconapp, fix activities

--- app/src/main/java/com/example/igor/whowantwillbemillioner/MainMenuActivity.java
+++ app/src/main/java/com/example/igor/whowantwillbemillioner/MainMenuActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.example.igor.whowantwillbemillioner.Entites.Question;
 
@@ -19,18 +21,24 @@ public class MainMenuActivity extends AppCompatActivity {
     }
 
     public void onClickExit(View view) {
-        moveTaskToBack(true);
+        Animation anim= AnimationUtils.loadAnimation(this,R.anim.buttonalpha);
+        view.startAnimation(anim);
+        //moveTaskToBack(true);
         finish();
-        System.exit(0);
+       // System.exit(0);
     }
 
     public void onClickStart(View view) {
-        int number = 0;
-        ArrayList<Question> questionList = new ArrayList<>();
-        questionList.addAll(Question.getQuestions());
+        Animation anim= AnimationUtils.loadAnimation(this,R.anim.buttonalpha);
+        view.startAnimation(anim);
+       // int number = 0;
+       // ArrayList<Question> questionList = new ArrayList<>();
+      // questionList.addAll(Question.getQuestions());
         Intent intent = new Intent(this, QuestionActivity.class);
-        intent.putExtra("Number", number);
-        intent.putExtra("QuestionList", questionList);
+        //intent.putExtra("Number", number);
+        //intent.putExtra("QuestionList", questionList);
         startActivity(intent);
+        overridePendingTransition(R.anim.animstart,R.anim.myanim);
+        finish();
     }
 }
